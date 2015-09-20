b750a337d4ddca5eaf20eea631e5125db89b86fe
Mohammed Hamendi <hamendi@gmail.com>
2015-09-20 12:02:20 +0200

    init

--- src/main/java/net/hamendi/HelloWorld.java
+++ src/main/java/net/hamendi/HelloWorld.java
@@ -1,9 +1,7 @@
 package net.hamendi;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -12,6 +10,7 @@ import javax.ws.rs.core.Response;
 // The Java class will be hosted at the URI path "/hello"
 @Path("/hello")
 public class HelloWorld {
+
     // The Java method will process HTTP GET requests
     @GET
     // The Java method will produce content identified by the MIME Media type "text/plain"
@@ -21,6 +20,46 @@ public class HelloWorld {
         String output = "Hello " + message + "!";
         return Response.status(200).entity(output).build();
     }
+
+    @GET
+    @Path("test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "Test";
+    }
+
+    /**
+     * Retrieves representation of an instance of testingwebapp.HelloWorld
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/json")
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of HelloWorld
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/json")
+    public void putJson(String content) {
+    }
+
+    /**
+     * Retrieves representation of an instance of helloWorld.HelloWorld
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("testhtml")
+    @Produces("text/html")
+    public String getHtml() {
+        return "<html lang=\"en\"><body><h1>Hello, World!!</body></h1></html>";
+    }
+
 
     /*public static void main(String[] args) throws IOException {
         HttpServer server = HttpServerFactory.create("http://localhost:9998/");
--- src/main/java/net/hamendi/servlet/RestJaxRsApp.java
+++
@@ -1,7 +0,0 @@
-package net.hamendi.servlet;
-
-/**
- * Created by gemini on 20/09/15.
- */
-public class RestJaxRsApp {
-}
